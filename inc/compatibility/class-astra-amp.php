3b8357479daa10b5019d141ea505b036e49fdde0
Balachandar <balachandark@bsf.io>
2018-12-07 11:03:38 +0530

    remove woocommerce sidebar integration

--- inc/compatibility/class-astra-amp.php
+++ inc/compatibility/class-astra-amp.php
@@ -45,7 +45,6 @@ if ( ! class_exists( 'Astra_AMP' ) ) :
 			add_filter( 'astra_nav_data_attrs', array( $this, 'add_nav_attrs' ) );
 			add_filter( 'astra_nav_toggle_data_attrs', array( $this, 'add_nav_toggle_attrs' ) );
 			add_filter( 'astra_caret_wrap_filter', array( $this, 'amp_dropdowns' ), 10, 2 );
-			add_filter( 'astra_sidebar_data_attrs', array( $this, 'add_woo_sidebar_attrs' ), 10, 2 );
 			add_action( 'wp_head', array( $this, 'render_amp_states' ) );
 		}
 
@@ -97,26 +96,6 @@ if ( ! class_exists( 'Astra_AMP' ) ) :
 			$input .= ' [class]="\'menu-toggle main-header-menu-toggle  ast-mobile-menu-buttons-minimal\' + ( astraAmpMenuExpanded ? \' active\' : \'\' )" ';
 			$input .= ' aria-expanded="false" ';
 			$input .= ' [aria-expanded]="astraAmpMenuExpanded ? \'true\' : \'false\'" ';
-			return $input;
-		}
-
-		/**
-		 * Add woo sidebar amp attrs.
-		 *
-		 * @param string $input input.
-		 * @param string $slug  sidebar slug.
-		 *
-		 * @return string
-		 */
-		public function add_woo_sidebar_attrs( $input, $slug ) {
-			if ( ! astra_amp_support() ) {
-				return $input;
-			}
-			if ( $slug !== 'shop-sidebar' ) {
-				return $input;
-			}
-			$input .= ' [class]="\'astra-sidebar-wrap ast-col-sm-12 left shop-sidebar \' + ( astraAmpWooSidebarExpanded ? \'sidebar-open\' : \'\' )" ';
-			$input .= ' aria-expanded="false" [aria-expanded]="astraAmpWooSidebarExpanded ? \'true\' : \'false\'" ';
 			return $input;
 		}
 
