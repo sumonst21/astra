61964f64f3c3096b47a151fa9ceaace876c7e694
premanshu <premanshup@bsf.io>
2019-03-15 12:28:13 +0530

    [GRUNT]

--- inc/addons/breadcrumbs/assets/js/unminified/customizer-preview.js
+++ inc/addons/breadcrumbs/assets/js/unminified/customizer-preview.js
@@ -21,32 +21,36 @@
 	astra_css( 'astra-settings[breadcrumb-font-family]', 'font-family', '.ast-breadcrumbs-wrapper .ast-breadcrumbs-name, .ast-breadcrumbs-wrapper .ast-breadcrumbs-item, .ast-breadcrumbs-wrapper .ast-breadcrumbs .separator' );
 	astra_css( 'astra-settings[breadcrumb-font-weight]', 'font-weight', '.ast-breadcrumbs-wrapper .ast-breadcrumbs-name, .ast-breadcrumbs-wrapper .ast-breadcrumbs-item, .ast-breadcrumbs-wrapper .ast-breadcrumbs .separator' );
 	astra_css( 'astra-settings[breadcrumb-text-transform]', 'text-transform', '.ast-breadcrumbs-wrapper .ast-breadcrumbs-name, .ast-breadcrumbs-wrapper .ast-breadcrumbs-item, .ast-breadcrumbs-wrapper .ast-breadcrumbs .separator' );
-	astra_css( 'astra-settings[breadcrumb-line-height]', 'line-height', '.ast-breadcrumbs-wrapper .ast-breadcrumbs-name, .ast-breadcrumbs-wrapper .ast-breadcrumbs-item, .ast-breadcrumbs-wrapper .ast-breadcrumbs .separator' );
-
+	
 	/* Yoast SEO Breadcrumb Color */
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-text-color-responsive]', 'color', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-active-color-responsive]', 'color', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-hover-color-responsive]', 'color', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a:hover' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-separator-color]', 'color', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
-
+	
 	/* Yoast SEO Breadcrumb Typography */
 	astra_responsive_font_size( 'astra-settings[breadcrumb-font-size]', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
 	astra_css( 'astra-settings[breadcrumb-font-family]', 'font-family', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
 	astra_css( 'astra-settings[breadcrumb-font-weight]', 'font-weight', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
 	astra_css( 'astra-settings[breadcrumb-text-transform]', 'text-transform', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
-	astra_css( 'astra-settings[breadcrumb-line-height]', 'line-height', '.ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span' );
-
+	
 	/* Breadcrumb NavXT Color */
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-text-color-responsive]', 'color', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-active-color-responsive]', 'color', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page.current-item' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-hover-color-responsive]', 'color', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page:hover, .ast-breadcrumbs-wrapper .breadcrumbs .home:hover' );
 	astra_color_responsive_css( 'breadcrumb', 'astra-settings[breadcrumb-separator-color]', 'color', '.ast-breadcrumbs-wrapper .breadcrumbs' );
-
+	
 	/* Breadcrumb NavXT Typography */
 	astra_responsive_font_size( 'astra-settings[breadcrumb-font-size]', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs' );
 	astra_css( 'astra-settings[breadcrumb-font-family]', 'font-family', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs' );
 	astra_css( 'astra-settings[breadcrumb-font-weight]', 'font-weight', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs' );
 	astra_css( 'astra-settings[breadcrumb-text-transform]', 'text-transform', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs' );
-	astra_css( 'astra-settings[breadcrumb-line-height]', 'line-height', '.ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs' );
+	
+	/* Breadcrumb default, Yoast SEO Breadcrumb, NavXT - Line Height */
+	astra_css( 
+		'astra-settings[breadcrumb-line-height]',
+		'line-height',
+		'.ast-breadcrumbs-wrapper .ast-breadcrumbs-name, .ast-breadcrumbs-wrapper .ast-breadcrumbs-item, .ast-breadcrumbs-wrapper .ast-breadcrumbs .separator, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast a, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast .breadcrumb_last, .ast-breadcrumbs-wrapper #ast-breadcrumbs-yoast span, .ast-breadcrumbs-wrapper .breadcrumbs .post-page, .ast-breadcrumbs-wrapper .breadcrumbs .home, .ast-breadcrumbs-wrapper .breadcrumbs'
+	);
 
 } )( jQuery );
